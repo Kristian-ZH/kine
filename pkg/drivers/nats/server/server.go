bfc9788edd6d6ab8a69d4acdefd05f5bde59f868
Kristian Zhelyazkov <kzhelyazkov@Kristians-MacBook-Pro.local>
2023-07-11 14:02:34 +0300

    Remove hardcoded configs

--- pkg/drivers/nats/server/server.go
+++ pkg/drivers/nats/server/server.go
@@ -2,9 +2,6 @@ package server
 
 import (
 	"fmt"
-	"math/rand"
-	"net/url"
-	"strconv"
 
 	"github.com/nats-io/nats-server/v2/server"
 )
@@ -40,33 +37,29 @@ func New(c *Config) (Server, error) {
 	// Explicitly set JetStream to true since we need the KV store.
 	opts.JetStream = true
 
-	opts.ServerName = "server-" + strconv.Itoa(rand.Intn(1000))
-	opts.StoreDir = "/root/nats/storage"
+	// opts.ServerName = "server-" + strconv.Itoa(rand.Intn(1000))
+	// opts.StoreDir = "/root/nats/storage"
 
-	opts.HTTPPort = 8000
+	// opts.Cluster = server.ClusterOpts{
+	// 	Name: "test-cluster",
+	// 	// Host: VM_IP,
+	// 	Port: 4248,
+	// }
 
-	// VM_IP := os.Getenv("VM_IP")
-
-	opts.Cluster = server.ClusterOpts{
-		Name: "test-cluster",
-		// Host: VM_IP,
-		Port: 4248,
-	}
-
-	opts.Routes = []*url.URL{
-		{
-			Host:   "192.168.64.41:4248",
-			Scheme: "nats",
-		},
-		{
-			Host:   "192.168.64.42:4248",
-			Scheme: "nats",
-		},
-		{
-			Host:   "192.168.64.43:4248",
-			Scheme: "nats",
-		},
-	}
+	// opts.Routes = []*url.URL{
+	// 	{
+	// 		Host:   "192.168.64.41:4248",
+	// 		Scheme: "nats",
+	// 	},
+	// 	{
+	// 		Host:   "192.168.64.42:4248",
+	// 		Scheme: "nats",
+	// 	},
+	// 	{
+	// 		Host:   "192.168.64.43:4248",
+	// 		Scheme: "nats",
+	// 	},
+	// }
 
 	opts.Debug = true
 
